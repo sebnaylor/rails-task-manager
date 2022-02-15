29855bf5bb8c47cc64218313a219920a9d72165b
Seb Naylor <sebnaylor@gmail.com>
2022-02-15 15:45:45 +0000

    Made a working tasks app. No css

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,18 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # index
+  get 'tasks', to: 'tasks#index'
+  # create
+  get 'tasks/new', to: 'tasks#new'
+  post 'tasks', to: 'tasks#create'
+
+  # read
+  get 'tasks/:id', to: 'tasks#show', as: :task
+
+  # update
+  get 'tasks/:id/edit', to: 'tasks#edit', as: :edit_task
+  patch 'tasks/:id', to: 'tasks#update'
+
+  # delete
+  delete 'tasks/:id/delete', to: 'tasks#destroy', as: :delete_task
+
 end
